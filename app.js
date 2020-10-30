ec9ea7bb480e41a7781a2fba4c3b51bd04607033
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2020-10-30 18:22:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,34 +6,13 @@ const WORD_ARRAY = [
     "fox",
     "jumped",
     "over",
-    "fence",
-    "university",
-    "zebra",
-    "country",
-    "mountain",
-    "cat",
-    "dog",
-    "because",
-    "of",
-    "human",
-    "dinosaur",
-    "television",
-    "radio",
-    "bike",
-    "car",
-    "girrafe",
-    "location",
-    "house",
-    "five",
-    "ten",
-    "program"
+    "fence"
 ];
 
 const wordDisplay = document.getElementById('word-display');
 const input = document.getElementById('input');
 const timer = document.getElementById('timer');
 const startButton = document.getElementById('start');
-//const restartButton = document.getElementById('restart');
 const results = document.querySelector('.results');
 const wpm = document.getElementById('wpm');
 
@@ -60,18 +39,12 @@ function generateRandomWords(){
     const randomWord = document.createElement('p');
     randomWord.innerText = WORD_ARRAY[Math.floor(Math.random() * WORD_ARRAY.length)] + " ";
     wordDisplay.appendChild(randomWord);
-    /*for(let i = 0; i < 10; i++){
-        const randomWord = document.createElement('p');
-        randomWord.innerText = WORD_ARRAY[Math.floor(Math.random() * WORD_ARRAY.length)] + " ";
-        wordDisplay.appendChild(randomWord);
-    }*/
 }
 
 startButton.addEventListener('click', setup);
 
 let currentWordCount = 0;
 let correctWords = 0;
-//let incorrectLetters = 0;
 let currentLength = 0;
 
 function checkText(){
@@ -79,14 +52,12 @@ function checkText(){
     const lastChar = input.value[input.value.length - 1];
 
     if(input.value.length < currentLength){
-        //incorrectLetters--;
         wordDisplay.children[currentWordCount].style.background = "none";
     }
 
     currentLength = input.value.length;
 
     if(lastChar != currentWord[input.value.length - 1]){
-        //incorrectLetters++;
         wordDisplay.children[currentWordCount].style.background = "red";
     }
 
@@ -112,22 +83,4 @@ function checkText(){
             generateRandomWords();
         }
     }
-}
-
-let time = 60;
-
-function countDown(){
-    time--;
-    timer.innerText = time;
-
-    if(time == 0){
-        clearInterval(countDownTimer);
-        showResults();
-        input.style.display = "none";
-    }
-}
-
-function showResults(){
-    results.style.display = "block";
-    wpm.innerText = correctWords;
 }
